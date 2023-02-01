3d8125ee9dc225317c6aa78afe36b9a389e85281
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-02-01 18:28:27 +0700

    Finish App

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import ContactList from '../components/ContactList';
+import SearchBar from '../components/SearchBar';
 import { deleteContact, getContacts } from '../utils/data';
+import { useSearchParams } from 'react-router-dom';
 
-class Homepage extends React.Component {
+function HomePageWrapper() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const keyword = searchParams.get('keyword');
+
+  function changeSearchParams(keyword) {
+    setSearchParams({ keyword });
+  }
+
+  return (
+    <HomePage defaultKeyword={keyword} keywordChange={changeSearchParams} />
+  );
+}
+
+class HomePage extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       contacts: getContacts(),
+      keyword: props.defaultKeyword || '',
     };
 
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this);
   }
 
   onDeleteHandler(id) {
     deleteContact(id);
-
     this.setState(() => {
       return {
         contacts: getContacts(),
@@ -23,17 +39,34 @@ class Homepage extends React.Component {
     });
   }
 
+  onKeywordChangeHandler(keyword) {
+    this.setState(() => {
+      return {
+        keyword,
+      };
+    });
+
+    this.props.keywordChange(keyword)
+  }
+
   render() {
+    const contacts = this.state.contacts.filter((contact) => {
+      return contact.name
+        .toLowerCase()
+        .includes(this.state.keyword.toLowerCase());
+    });
+
     return (
       <section>
-        <h2>Daftar Kontak</h2>
-        <ContactList
-          contacts={this.state.contacts}
-          onDelete={this.onDeleteHandler}
+        <SearchBar
+          keyword={this.state.keyword}
+          keywordChange={this.onKeywordChangeHandler}
         />
+        <h2>Daftar Kontak</h2>
+        <ContactList contacts={contacts} onDelete={this.onDeleteHandler} />
       </section>
     );
   }
 }
 
-export default Homepage;
+export default HomePageWrapper;
